caa5ff5bcf6dfb08adb04435f909ea257e25167c
istrackerdown <istrackerdown@gmail.com>
2016-04-28 11:54:07 -0400

    Added script

---
+++ test.py
@@ -0,0 +1,30 @@
+import requests
+import time
+
+derp = 0
+''' function to monitor up time '''
+while True:
+    try:
+        r = requests.get('http://tracker2.bfscapital.com', timeout=20)
+        if r.status_code == 200:
+            if derp == 0:
+                time.sleep(60)
+            elif derp == 1:
+                print "Tracker is back up"
+                time.sleep(60)
+                derp = 0
+        else:
+            if derp == 1:
+                time.sleep(60)
+            elif derp == 0:
+                print "Tracker is down"
+                time.sleep(60)
+                derp = 1
+
+    except requests.Timeout:
+        if derp == 1:
+            time.sleep(60)
+        elif derp == 0:
+            print "Tracker is down"
+            time.sleep(60)
+            derp = 1
