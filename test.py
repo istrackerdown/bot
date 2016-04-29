a42226cff04b831137a144ca1d0ef9de1e6bb98c
istrackerdown <istrackerdown@gmail.com>
2016-04-29 15:22:48 -0400

    Added multithreading, Modified change handler variable

--- test.py
+++ test.py
@@ -1,32 +1,49 @@
+from threading import Thread
 import requests
 import time
 import twitter
 
-derp = 0
-''' function to monitor up time '''
-while True:
-    try:
-        r = requests.get('http://tracker.bfscapital.com', timeout=20)
-        if r.status_code == 200:
-            if derp == 0:
-                print "Tracker is up"
-                time.sleep(60)
-            elif derp == 1:
-                print "Tracker is back up"
-                time.sleep(60)
-                derp = 0
-        else:
-            if derp == 1:
-                time.sleep(60)
-            elif derp == 0:
-                print "Tracker is down"
-                time.sleep(60)
-                derp = 1
+#def tweet():
 
-    except requests.Timeout:
-        if derp == 1:
-            time.sleep(60)
-        elif derp == 0:
-            print "Tracker is down"
-            time.sleep(60)
-            derp = 1
+def test(siteurl, sitename):
+    currentstatus = 1
+    while True:
+        try:
+            r = requests.get(siteurl, timeout=20)
+            if r.status_code == 200:
+                if currentstatus == 1:
+                    print sitename, "is still up"
+                    time.sleep(60)
+                else:
+                    print sitename, "is back up"
+                    time.sleep(60)
+                    currentstatus = 1
+            else:
+                if currentstatus == 0:
+                    print sitename, "is still down"
+                    time.sleep(60)
+                else:
+                    print sitename, "is down"
+                    time.sleep(60)
+                    currentstatus = 0
+
+        except requests.Timeout:
+            if currentstatus == 1:
+                time.sleep(60)
+            elif currentstatus == 0:
+                print sitename, "is down"
+                time.sleep(60)
+                currentstatus = 1
+
+def tracker():
+    test('http://tracker.bfscapital.com', 'tracker')
+
+def docloc():
+    test('https://documents.bfscapital.com', 'doclocator')
+
+thread1 = Thread(target = tracker)
+thread2 = Thread(target = docloc)
+
+if __name__ == '__main__':
+    thread1.start()
+    thread2.start()
